46e77ba159da8bc2c7e6621ab2ae45ebacc9a647
“CasCCode” <estebancastro@javerianacali.edu.co>
2024-11-15 16:47:34 -0500

    Agrega el funcionamiento básico de la consolidación. Falta filtro de programas

--- src/gestorArchivos.py
+++ src/gestorArchivos.py
@@ -7,9 +7,11 @@ class GestorArchivos:
 
     def leer_archivo(self, RUTA_BASE, anio, dict_programas_academicos, atributo_del_archivo, primera_vez):
 
-        ruta_completa = RUTA_BASE + atributo_del_archivo + anio + ".csv"
+        ruta_completa = RUTA_BASE + atributo_del_archivo + anio + ".xlsx"
         print(f"La ruta del archivo es \n{ruta_completa}")
-        df = pd.read_csv(ruta_completa, sep=";", encoding='utf-8')
+        df = pd.read_excel(ruta_completa)
+        df = self.convertir_columna_to_int64(df)
+
 
         list_cod_snies = list(dict_programas_academicos.keys())
 
@@ -24,46 +26,28 @@ class GestorArchivos:
             if anio not in dict_programas_academicos[cod_snies].dict_consolidados.keys():
                 consolidado_actual_obj = Consolidado()
                 consolidado_actual_obj.consolidado = df_filtrado.iloc[:, index_columna_inicio_exclusion:]
-                consolidado_actual_obj.consolidado["INSCRITOS"] = None
-                consolidado_actual_obj.consolidado["MATRICULADOS"] = None
-                consolidado_actual_obj.consolidado["PRIMER CURSO"] = None
-                consolidado_actual_obj.consolidado["GRADUADOS"] = None
+                consolidado_actual_obj.consolidado = consolidado_actual_obj.consolidado.reset_index(drop=True)
                 dict_programas_academicos[cod_snies].dict_consolidados[anio] = consolidado_actual_obj
 
 
         for cod_snies in list_cod_snies:
             df_filtrado = df[df["CÓDIGO SNIES DEL PROGRAMA"] == cod_snies]
+            df_filtrado = df_filtrado.reset_index(drop=True)
 
             if atributo_del_archivo == "admitidos":
                 dict_programas_academicos[cod_snies].dict_consolidados[anio].consolidado.loc[:, "ADMITIDOS"] = df_filtrado.loc[:, "ADMITIDOS"]
-                #print(f"Editando el consolidado del año {anio} del programa: {cod_snies}")
 
-            elif atributo_del_archivo == "inscritos":
-                dict_programas_academicos[cod_snies].dict_consolidados[anio].consolidado.loc[:, "INSCRITOS"] = df_filtrado.loc[:, "INSCRITOS"]
-                print(f"Editando el consolidado del año {anio} del programa: {cod_snies}")
+            if atributo_del_archivo == "inscritos":
+                dict_programas_academicos[cod_snies].dict_consolidados[anio].consolidado.loc[:, "INSCRITOS"] = df_filtrado["INSCRITOS"]
 
-            elif atributo_del_archivo == "matriculados":
+            if atributo_del_archivo == "matriculados":
                 dict_programas_academicos[cod_snies].dict_consolidados[anio].consolidado.loc[:, "MATRICULADOS"] = df_filtrado.loc[:, "MATRICULADOS"]
 
-            elif atributo_del_archivo == "matriculadosPrimerSemestre":
+            if atributo_del_archivo == "matriculadosPrimerSemestre":
                 dict_programas_academicos[cod_snies].dict_consolidados[anio].consolidado.loc[:, "PRIMER CURSO"] = df_filtrado.loc[:, "PRIMER CURSO"]
 
-            elif atributo_del_archivo == "graduados":
+            if atributo_del_archivo == "graduados":
                 dict_programas_academicos[cod_snies].dict_consolidados[anio].consolidado.loc[:, "GRADUADOS"] = df_filtrado.loc[:, "GRADUADOS"]
-
-        """
-        
-            elif atributo_del_archivo == "inscritos":
-                dict_programas_academicos[cod_snies]["INSCRITOS"] = df_filtrado["INSCRITOS"]
-            elif atributo_del_archivo == "matriculados":
-                dict_programas_academicos[cod_snies]["MATRICULADOS"] = df_filtrado["MATRICULADOS"]
-            elif atributo_del_archivo == "neos":
-                dict_programas_academicos[cod_snies]["NEOS"] = df_filtrado["NEOS"]
-            elif atributo_del_archivo == "graduados":
-                dict_programas_academicos[cod_snies]["GRADUADOS"] = df_filtrado["GRADUADOS"]
-            """
-        df_filtrado = 0
-
 
 
     def exportar_archivo(self, dict_programas_academicos):
@@ -96,5 +80,13 @@ class GestorArchivos:
         df_final = pd.concat(lista_programas_academicos_a_combinar, ignore_index=True)
 
         # FIXME: MANEJAR EL CASO EN QUE PROBABLEENTE ESTÉ VACÍO EL DATAFRAME
-        df_final.to_csv("Resultados.csv", sep=";", index=False, encoding="utf-8-sig")
+        df_final.to_excel("Resultados.xlsx")
         print("Probando")
+
+    def convertir_columna_to_int64(self, df):
+        df["CÓDIGO SNIES DEL PROGRAMA"] = df["CÓDIGO SNIES DEL PROGRAMA"].astype(str).str.strip()
+        df["CÓDIGO SNIES DEL PROGRAMA"] = pd.to_numeric(df["CÓDIGO SNIES DEL PROGRAMA"], errors='coerce')
+        df.dropna(subset=["CÓDIGO SNIES DEL PROGRAMA"], inplace=True)
+        df["CÓDIGO SNIES DEL PROGRAMA"] = df["CÓDIGO SNIES DEL PROGRAMA"].astype("int64")
+
+        return df
